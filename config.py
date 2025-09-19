b3f44b3bae0b40849f2de5292832fa8f4b0332ca
aiaicom <rovotev@gmail.com>
2025-09-19 15:39:59 +0900

    prepare for deploy

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7eb55db46d26cbf1934f50b4245894fe21a2e28aba2328f2aa894b18abc6caa3
-size 3900
+oid sha256:c341918e1d67cd4b279bcf3a91458ab1432da155fad30e2dd292b070b3ff7ac8
+size 3957
